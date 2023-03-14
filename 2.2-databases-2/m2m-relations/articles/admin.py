3262e5fb0dfdc0df057998bbe83e84efe085dd12
Vladislav Mashin <mashinvladislav@gmail.com>
2023-03-14 14:15:23 +0300

    update 1.2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -11,17 +11,11 @@ class ArticleThemeInlineFormset(BaseInlineFormSet):
 
         count_main = 0
         for form in self.forms:
-            # В form.cleaned_data будет словарь с данными
-            # каждой отдельной формы, которые вы можете проверить
             if form.cleaned_data['is_main']:
                 count_main += 1
             if count_main > 1:
                 raise ValidationError('Главная рубрика отмечена более одного раза!')
-            # вызовом исключения ValidationError можно указать админке о наличие ошибки
-            # таким образом объект не будет сохранен,
-            # а пользователю выведется соответствующее сообщение об ошибке
-            # raise ValidationError('Тут всегда ошибка')
-        return super().clean()  # вызываем базовый код переопределяемого метода
+        return super().clean()
 
 class ArticleThemeInline(admin.TabularInline):
     model = ArticleTheme
