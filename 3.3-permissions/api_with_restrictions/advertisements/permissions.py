bfae9ff02dc98f0a738cf5155d461ea707368c6a
Vladislav Mashin <mashinvladislav@gmail.com>
2023-04-04 12:03:45 +0300

    update 1.2

--- 3.3-permissions/api_with_restrictions/advertisements/permissions.py
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -1,14 +1,17 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class CreateOrReadOnly(BasePermission):
-
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-        return request.user and request.user.is_authenticated
-
+class IsAdvertisementOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return request.user == obj.creator
+        return request.user.is_authenticated and obj.creator == request.user
+# class CreateOrReadOnly(BasePermission):
+#
+#     def has_permission(self, request, view):
+#         if request.method in SAFE_METHODS:
+#             return True
+#         return request.user and request.user.is_authenticated
+#
+#     def has_object_permission(self, request, view, obj):
+#         if request.method in SAFE_METHODS:
+#             return True
+#         return request.user == obj.creator
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -5,7 +5,7 @@ from rest_framework.viewsets import ModelViewSet
 from .filters import AdvertisementFilter
 from .models import Advertisement
 from advertisements.serializers import AdvertisementSerializer
-from .permissions import CreateOrReadOnly
+from .permissions import IsAdvertisementOwner
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -20,6 +20,8 @@ class AdvertisementViewSet(ModelViewSet):
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action == "create":
             return [IsAuthenticated()]
+        elif self.action in ["update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsAdvertisementOwner()]
         return []
